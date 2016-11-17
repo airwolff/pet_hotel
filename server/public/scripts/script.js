2296161c70bfa0497ed5d38b761814e5973a28f8
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-17 16:35:53 -0600

    Add registerOwner function

---
+++ server/public/scripts/script.js
@@ -0,0 +1,27 @@
+function registerOwner() {
+  event.preventDefault();
+
+  // owner object will contain owner first name and last name
+  var owner = {};
+
+  $.each($('#owner-form').serializeArray(), function (i, field) {
+    owner[field.name] = field.value;
+  });
+
+
+
+  console.log('book: ', book);
+
+  $.ajax({
+    type: 'POST',
+    url: '/owners',
+    data: owner,
+    success: function(response) {
+      console.log('success on client side')
+    },
+    error: function() {
+      console.log('could not register a new owner');
+    }
+  })
+
+}
