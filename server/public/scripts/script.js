58fa142c036c2931a34df1d9fd177b0dcf21733b
Joseph Giwoyna <josephgiwoyna@Josephs-MacBook-Pro-2.local>
2016-11-17 17:04:57 -0600

    added pets function

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -25,3 +25,31 @@ function registerOwner() {
   })
 
 }
+
+function registerPets() {
+  event.preventDefault();
+
+  // owner object will contain owner first name and last name
+  var pets = {};
+
+  $.each($('#pets-form').serializeArray(), function (i, field) {
+    pets[field.name] = field.value;
+  });
+
+
+
+  console.log('pets: ', pets);
+
+  $.ajax({
+    type: 'POST',
+    url: '/pets',
+    data: pets,
+    success: function(response) {
+      console.log('success on client side')
+    },
+    error: function() {
+      console.log('could not register a new pet');
+    }
+  })
+
+}
